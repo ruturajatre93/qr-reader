1ed1b8267c3da6fbc58d1c08232ec663634f1caa
Ruturaj Atre <ruturaj.atre@gmail.com>
2020-01-28 01:09:11 +0530

    Updates

---
+++ precache-manifest.d62b2f858ffe90c15548a7c05d5bfcc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f90432c80ea1e20c1ccce5c6d491e28",
+    "url": "/qr-reader/index.html"
+  },
+  {
+    "revision": "03e4dfd6953555fdf69d",
+    "url": "/qr-reader/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1ad8c370c0889a7db7ee",
+    "url": "/qr-reader/static/js/2.b3dced64.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/qr-reader/static/js/2.b3dced64.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03e4dfd6953555fdf69d",
+    "url": "/qr-reader/static/js/main.852b2612.chunk.js"
+  },
+  {
+    "revision": "12621912f0242969253c",
+    "url": "/qr-reader/static/js/runtime-main.b3ae3826.js"
+  }
+]);
